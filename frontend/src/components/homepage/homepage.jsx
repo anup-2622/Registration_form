31583b27ff97da9855367cd8d934dafde72e687b
Anup <anupisri107@gmail.com>
2023-06-01 11:52:15 +0530

    homepage

---
+++ frontend/src/components/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./home.scss";
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      <h1>hello homepage </h1>
+      <button className="btn">Logout</button>
+    </div>
+  );
+};
+
+export default Homepage;
